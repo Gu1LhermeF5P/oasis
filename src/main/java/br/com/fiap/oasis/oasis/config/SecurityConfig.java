9936982f083a9c5e52f684c54382c48af3cb0127
Guilherme Pereira <gfranciscosilvapereira@gmail.com>
2025-11-18 10:40:35 -0300

    remove imagem de logo e estiliza tipografia com padrão Thymeleaf

--- src/main/java/br/com/fiap/oasis/oasis/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/oasis/oasis/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 public class SecurityConfig {
 
@@ -13,20 +14,23 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                // Libera o acesso à página de login e arquivos estáticos (CSS/JS)
-                .requestMatchers("/login", "/css/**", "/js/**").permitAll()
-                // O resto continua bloqueado
+                
+                .requestMatchers("/login", "/css/**", "/js/**", "/images/**").permitAll()
+                
+                .requestMatchers("/h2-console/**").permitAll()
+               
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form
-                .loginPage("/login")  // <--- AQUI ESTÁ A MÁGICA
-                .defaultSuccessUrl("/app/rituais", true) // Para onde vai depois de logar?
+                .loginPage("/login")
+                .defaultSuccessUrl("/app/rituais", true)
                 .permitAll()
             )
             .logout(logout -> logout
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/login?logout")
-            );
+            )
+            .headers(headers -> headers.frameOptions(frame -> frame.disable()));
         
         return http.build();
     }
