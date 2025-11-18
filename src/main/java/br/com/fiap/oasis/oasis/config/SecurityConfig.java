f5f471638b780cbc039cff0e8490696aabd69a69
Guilherme Pereira <gfranciscosilvapereira@gmail.com>
2025-11-18 10:06:47 -0300

    config: configure custom form login page in SecurityConfig

--- src/main/java/br/com/fiap/oasis/oasis/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/oasis/oasis/config/SecurityConfig.java
@@ -4,21 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable()) // API REST (Stateless) 
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                // Permite acesso livre à documentação (Swagger)
-                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                
+                // Libera o acesso à página de login e arquivos estáticos (CSS/JS)
+                .requestMatchers("/login", "/css/**", "/js/**").permitAll()
+                // O resto continua bloqueado
                 .anyRequest().authenticated()
             )
-            .httpBasic(withDefaults()); 
+            .formLogin(form -> form
+                .loginPage("/login")  // <--- AQUI ESTÁ A MÁGICA
+                .defaultSuccessUrl("/app/rituais", true) // Para onde vai depois de logar?
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+            );
+        
         return http.build();
     }
 }
